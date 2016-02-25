eba792e233f7fd3a3f800c6fcd4d25d2e988d549
hoergems <hoergems@gmail.com>
2016-02-25 18:17:27 +1000

    Added remaining mpc scripts

---
+++ mpc10.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#SBATCH --job-name=mpc10
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=2096
+#SBATCH --array=1-2
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hoergems@gmail.com
+
+source /home/hoe01h/.bash_profile
+cd /data/hoe01h/LQG/LQG10_$SLURM_ARRAY_TASK_ID/
+python mpc.py
---
+++ mpc8.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#SBATCH --job-name=mpc8
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=2096
+#SBATCH --array=1-2
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hoergems@gmail.com
+
+source /home/hoe01h/.bash_profile
+cd /data/hoe01h/LQG/LQG8_$SLURM_ARRAY_TASK_ID/
+python mpc.py
