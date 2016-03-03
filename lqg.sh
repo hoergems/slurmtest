ccfc9a312a0caeeca5f3a0a41688935ca38dadab
hoergems <hoergems@gmail.com>
2016-03-03 17:11:01 +1000

    Added lqg

---
+++ lqg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#SBATCH --job-name=lqg
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=2048
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hoergems@gmail.com
+
+source /home/hoe01h/.bash_profile
+cd /data/hoe01h/LQGActual/LQG/
+python lqg -d
